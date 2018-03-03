58f896407922688ff75b8d6bfd15bb4b64223726
Yusaku ONO <yono.05@gmail.com>
2018-03-03 14:38:22 +0900

    Tweak routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#home'
   resources :channels, only: :index do
-    resources :talks, only: [:index, :create]
+    resources :talks, only: %i(index create)
   end
   resource :sessions, only: %i(new create destroy)
 end
