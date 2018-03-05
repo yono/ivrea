8dfa7b49b5c80ea84e5d6d5acb90c101901df208
Yusaku ONO <yono.05@gmail.com>
2018-03-05 23:55:09 +0900

    Clean up controllers

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,8 +1,6 @@
 class ChannelsController < ApplicationController
   def index
     @channels = Channel.all
-    respond_to do |format|
-      format.json { render json: @channels }
-    end
+    render json: @channels
   end
 end
--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -3,21 +3,7 @@ class TalksController < ApplicationController
     raise unless params[:channel_id]
     channel = Channel.find params[:channel_id]
     @talks = channel.talks
-    respond_to do |format|
-      format.json { render json: @talks }
-    end
-  end
-
-  def create
-    channel = Channel.find params[:channel_id]
-    @talk = channel.talks.new(talk_params.merge(user_name: session[:user]))
-    if @talk.save
-      respond_to do |format|
-        format.json { render json: @talk, status: :created}
-      end
-    else
-      puts @talk.errors.full_messages
-    end
+    render json: @talks
   end
 
   private
