a6793c4e036b0dc013c61c5c0b332508539ab852
Yusaku ONO <yono.05@gmail.com>
2018-03-08 09:01:30 +0900

    Fix login page design

---
+++ app/javascript/packs/login.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+
+function Login() {
+  const token = document.querySelector('meta[name=csrf-token]').getAttribute('content')
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <Paper style={{padding: '20px', marginTop: '40px'}}>
+          <Typography>
+            Login
+          </Typography>
+          <form action='/sessions' method='post' style={{margin: 'auto'}}>
+            <TextField
+              id='session_user'
+              name='session[user]'
+              placeholder='User Name'
+            />
+            <input type='hidden' name='authenticity_token' value={token} />
+            <Button variant='raised' color='primary' type='submit'>
+              Start
+            </Button>
+          </form>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}></Grid>
+      </Grid>
+    </div>
+  );
+}
+
+let e = document.createElement('div')
+e.className = 'root'
+render(<Login />, document.body.appendChild(e));
