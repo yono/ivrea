93900805d3ee14eef5408f95587bbb977efab167
Yusaku ONO <yono.05@gmail.com>
2018-05-29 00:55:10 +0900

    Add uniqueness to email

--- db/migrate/20180528150506_create_users.rb
+++ db/migrate/20180528150506_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :password_digest, null: false
 
       t.timestamps
+
+      t.index :email, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180528150506) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
