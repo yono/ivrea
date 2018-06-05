760ea88ff36e8cfe37bd275915bdefcb94edc29f
Yusaku ONO <yono.05@gmail.com>
2018-06-05 23:01:40 +0900

    setting for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'ivrea.heroku.com',
-    :authentication => :plain,
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'ivrea.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
-  ActionMailer::Base.delivery_method = :smtp
 end
